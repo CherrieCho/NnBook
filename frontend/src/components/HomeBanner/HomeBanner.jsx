bc938744dbdb19b035dbd3d5dfa9c1acf872f746
JiHyoung You <fbwlgud51@gmail.com>
2025-05-23 17:00:38 +0900

    style: 홈 배너 swiper 적용

--- frontend/src/components/HomeBanner/HomeBanner.jsx
+++ frontend/src/components/HomeBanner/HomeBanner.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
 import "./styles/HomeBanner.style.css";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay, Pagination } from "swiper/modules";
+import banner1 from "../../assets/Sample-Banner1.png";
+import banner2 from "../../assets/Sample-Banner2.png";
+import banner3 from "../../assets/Sample-Banner3.png";
 
 const HomeBanner = () => {
   return (
-    <div className='home-banner-container'></div>
-  )
-}
+    <div className="home-banner-container">
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+      >
+        <SwiperSlide>
+          <img src={banner1} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner2} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner3} />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
 
-export default HomeBanner
+export default HomeBanner;
